c6a293d44600a71aa231ee2f5c15bfd4ab15e9c8
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-01-30 01:16:08 +0600

    handle large form with useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import LargeFormWithReducer from './components/LargeFormWithReducer';
 import SimpleCounterWithUseReducer from './components/SimpleCounterWithUseReducer';
+
 
 function App() {
   return (
     <div className="container">
       {/* <SimpleCounterWithUseReducer/> */}
       <LargeFormWithReducer />
+   
     </div>
   );
 }
--- src/components/LargeFormWithReducer.js
+++ src/components/LargeFormWithReducer.js
@@ -11,29 +11,38 @@ const LargeFormWithReducer = () => {
     password: "",
     address: "",
     city: "",
-    city: "",
     course: "",
-    gander: null,
-    checked: false
+    gender: null,
+    checked: false,
+    quantity: 0,
+    message: "",
   };
 
   const reducer = (state, action) => {
-    console.log(action)
-    if(action.type == "INPUT"){
-      
+    console.log(action);
 
+    switch (action.type) {
+      case "INPUT":
+        return { ...state, [action.payLoad.name]: action.payLoad.value };
+      case "TOGGLE":
+        return { ...state, checked: !state.checked };
+      case "INCREMENT":
+        return { ...state, quantity: state.quantity +1 };
+      case "DECREMENT":
+        return { ...state, quantity: state.quantity -1 };
+      default:
+        return state;
     }
-
   };
 
-  const submit = (event)=>{
+  const submit = (event) => {
     event.preventDefault();
 
-    console.log(state)
-
-  }
+    console.log(state);
+  };
 
   const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="container mt-5">
       <form onSubmit={submit} className="row g-3 bg-success p-4 rounded">
@@ -41,34 +50,85 @@ const LargeFormWithReducer = () => {
           <label htmlFor="inputFirstName" className="form-label">
             First Name
           </label>
-          <input type="text" onChange={(event)=> dispatch({type: "INPUT", payLoad: {value: event.target.value}})} className="form-control" id="inputFirstName" />
+          <input
+            type="text"
+            name="firstName"
+            onBlur={(event) =>
+              dispatch({
+                type: "INPUT",
+                payLoad: { value: event.target.value, name: event.target.name },
+              })
+            }
+            className="form-control"
+            id="inputFirstName"
+          />
         </div>
         <div className="col-md-6">
           <label htmlFor="inputLastName" className="form-label">
             Last Name
           </label>
-          <input type="text" className="form-control" id="inputLastName" />
+          <input
+            name="lastName"
+            type="text"
+            onBlur={(event) =>
+              dispatch({
+                type: "INPUT",
+                payLoad: { value: event.target.value, name: event.target.name },
+              })
+            }
+            className="form-control"
+            id="inputLastName"
+          />
         </div>
         <div className="col-md-6">
           <label htmlFor="inputEmail4" className="form-label">
             Email
           </label>
-          <input type="email" className="form-control" id="inputEmail4" />
+          <input
+            type="email"
+            onBlur={(event) =>
+              dispatch({
+                type: "INPUT",
+                payLoad: { value: event.target.value, name: event.target.name },
+              })
+            }
+            name="email"
+            className="form-control"
+            id="inputEmail4"
+          />
         </div>
         <div className="col-md-6">
           <label htmlFor="inputPassword4" className="form-label">
             Password
           </label>
-          <input type="password" className="form-control" id="inputPassword4" />
+          <input
+            type="password"
+            name="password"
+            onBlur={(event) =>
+              dispatch({
+                type: "INPUT",
+                payLoad: { value: event.target.value, name: event.target.name },
+              })
+            }
+            className="form-control"
+            id="inputPassword4"
+          />
         </div>
         <div className="col-12">
           <label htmlFor="inputAddress" className="form-label">
             Address
           </label>
           <input
+            name="address"
             type="text"
             className="form-control"
             id="inputAddress"
+            onBlur={(event) =>
+              dispatch({
+                type: "INPUT",
+                payLoad: { value: event.target.value, name: event.target.name },
+              })
+            }
             placeholder="1234 Main St"
           />
         </div>
@@ -77,43 +137,84 @@ const LargeFormWithReducer = () => {
           <label htmlFor="inputCity" className="form-label">
             City
           </label>
-          <input type="text" className="form-control" id="inputCity" />
+          <input
+            onBlur={(event) =>
+              dispatch({
+                type: "INPUT",
+                payLoad: { value: event.target.value, name: event.target.name },
+              })
+            }
+            name="city"
+            type="text"
+            className="form-control"
+            id="inputCity"
+          />
         </div>
         <div className="col-12">
           <label htmlFor="inputState" className="form-label">
             Course
           </label>
-          <select id="inputState" className="form-select">
+          <select
+            id="inputState"
+            onChange={(event) =>
+              dispatch({
+                type: "INPUT",
+                payLoad: { value: event.target.value, name: event.target.name },
+              })
+            }
+            name="course"
+            className="form-select"
+          >
             <option disabled>Choose...</option>
-            <option>JS</option>
-            <option>Nodejs</option>
-            <option>Express</option>
-            <option>React</option>
-            <option>Electron</option>
-            <option>NEXT</option>
-            <option>NEST</option>
+            <option value={"JS"}>JS</option>
+            <option value={"Nodejs"}>Nodejs</option>
+            <option value={"Express"}>Express</option>
+            <option value={"React"}>React</option>
+            <option value={"Electron"}>Electron</option>
+            <option value={"NEXT"}>NEXT</option>
+            <option value={"NEST"}>NEST</option>
           </select>
         </div>
         <div className="col-12">
-          <div class="form-check">
+          <div className="form-check">
             <input
-              class="form-check-input"
+              onChange={(event) =>
+                dispatch({
+                  type: "INPUT",
+                  payLoad: {
+                    value: event.target.value,
+                    name: event.target.name,
+                  },
+                })
+              }
+              className="form-check-input"
               type="radio"
-              name="flexRadioDefault"
+              value="male"
+              name="gender"
               id="flexRadioDefault1"
             />
-            <label class="form-check-label" for="flexRadioDefault1">
+            <label className="form-check-label" for="flexRadioDefault1">
               Male
             </label>
           </div>
-          <div class="form-check">
+          <div className="form-check">
             <input
-              class="form-check-input"
+              className="form-check-input"
               type="radio"
-              name="flexRadioDefault"
+              name="gender"
+              value="female"
+              onChange={(event) =>
+                dispatch({
+                  type: "INPUT",
+                  payLoad: {
+                    value: event.target.value,
+                    name: event.target.name,
+                  },
+                })
+              }
               id="flexRadioDefault2"
             />
-            <label class="form-check-label" for="flexRadioDefault2">
+            <label className="form-check-label" for="flexRadioDefault2">
               Female
             </label>
           </div>
@@ -124,6 +225,8 @@ const LargeFormWithReducer = () => {
               className="form-check-input"
               type="checkbox"
               id="gridCheck"
+              name="checked"
+              onClick={(event)=> dispatch({type: "TOGGLE"})}
             />
             <label className="form-check-label" htmlFor="gridCheck">
               Check me out
@@ -131,10 +234,45 @@ const LargeFormWithReducer = () => {
           </div>
         </div>
         <div className="col-12">
-          <button type="submit" className="btn btn-primary">
-            Sign in
-          </button>
+          <label for="exampleFormControlTextarea1" className="form-label">
+            Example textarea
+          </label>
+          <textarea
+            name="message"
+            onBlur={(event) =>
+              dispatch({
+                type: "INPUT",
+                payLoad: {
+                  value: event.target.value,
+                  name: event.target.name,
+                },
+              })
+            }
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
         </div>
+        <div className="col-12">
+          <div className="text-center">
+            <p>{state.quantity}</p>
+            <div>
+              <button onClick={()=> dispatch({type:"DECREMENT"})} type="button" className="btn btn-primary me-4">
+                -
+              </button>
+              <button onClick={()=> dispatch({type:"INCREMENT"})} type="button" className="btn btn-primary">
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+        {state.checked && (
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary">
+              Sign in
+            </button>
+          </div>
+        )}
       </form>
     </div>
   );
